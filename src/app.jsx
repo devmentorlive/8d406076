be08fbcaa05fc45bc3903c2a06613a81ff207d35
Mark Tellez <mark.tellez@icloud.com>
2020-07-06 00:17:36 -0500

    done

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Datatable from "./datatable";
+
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
 export default function App() {
-  return <div>Start here</div>;
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState("");
+  const [searchColumns, setSearchColumns] = useState(["firstName", "lastName"]);
+
+  useEffect(() => {
+    fetch("https://devmentor.live/api/examples/contacts?api_key=b7c58b")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  function search(rows) {
+    return rows.filter((row) =>
+      searchColumns.some(
+        (column) =>
+          row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
+      )
+    );
+  }
+
+  const columns = data[0] && Object.keys(data[0]);
+  return (
+    <div>
+      <div>
+        <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+        {columns &&
+          columns.map((column) => (
+            <label>
+              <input
+                type="checkbox"
+                checked={searchColumns.includes(column)}
+                onChange={(e) => {
+                  const checked = searchColumns.includes(column);
+                  setSearchColumns((prev) =>
+                    checked
+                      ? prev.filter((sc) => sc !== column)
+                      : [...prev, column]
+                  );
+                }}
+              />
+              {column}
+            </label>
+          ))}
+      </div>
+      <div>
+        <Datatable data={search(data)} />
+      </div>
+    </div>
+  );
 }
